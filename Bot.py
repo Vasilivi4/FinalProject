759387a984faa39cc1d6dd343042a28d4b3b3876
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2023-11-02 07:13:27 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,44 +6,94 @@ class Bot:
         self.book = AddressBook()
 
     def handle(self, action):
-        if action == 'add':
-            name = Name(input("Name: ")).value.strip()
-            phones = Phone().value
-            birth = Birthday().value
-            email = Email().value.strip()
-            status = Status().value.strip()
-            note = Note(input("Note: ")).value
-            record = Record(name, phones, birth, email, status, note)
-            return self.book.add(record)
-        elif action == 'search':
-            print("There are following categories: \nName \nPhones \nBirthday \nEmail \nStatus \nNote")
-            category = input('Search category: ')
-            pattern = input('Search pattern: ')
-            result = (self.book.search(pattern, category))
-            for account in result:
-                if account['birthday']:
-                    birth = account['birthday'].strftime("%d/%m/%Y")
-                    result = "_" * 50 + "\n" + f"Name: {account['name']} \nPhones: {', '.join(account['phones'])} \nBirthday: {birth} \nEmail: {account['email']} \nStatus: {account['status']} \nNote: {account['note']}\n" + "_" * 50
-                    print(result)
-        elif action == 'edit':
-            contact_name = input('Contact name: ')
-            parameter = input('Which parameter to edit(name, phones, birthday, status, email, note): ').strip()
-            new_value = input("New Value: ")
-            return self.book.edit(contact_name, parameter, new_value)
-        elif action == 'remove':
-            pattern = input("Remove (contact name or phone): ")
-            return self.book.remove(pattern)
-        elif action == 'save':
-            file_name = input("File name: ")
-            return self.book.save(file_name)
-        elif action == 'load':
-            file_name = input("File name: ")
-            return self.book.load(file_name)
-        elif action == 'congratulate':
-            print(self.book.congratulate())
-        elif action == 'view':
-            print(self.book)
-        elif action == 'exit':
+        if action == "add":
+            self.add_contact()
+        elif action == "search":
+            self.search_contacts()
+        elif action == "edit":
+            self.edit_contact()
+        elif action == "remove":
+            self.remove_contact()
+        elif action == "save":
+            self.save_to_file()
+        elif action == "load":
+            self.load_from_file()
+        elif action == "congratulate":
+            self.congratulate()
+        elif action == "view":
+            self.view_contacts()
+        elif action == "exit":
             pass
         else:
-            print("There is no such command!")
+            print("No such command!")
+
+    def add_contact(self):
+        name = Name(input("Name: ")).value.strip()
+        phones = Phone().value
+        birth = Birthday().value
+        email = Email().value.strip()
+        status = Status().value.strip()
+        note = Note(input("Note: ")).value
+        record = Record(name, phones, birth, email, status, note)
+        self.book.add(record)
+
+    def search_contacts(self):
+        print(
+            "Available categories for search: \nName \nPhones \nBirthday \nEmail \nStatus \nNote"
+        )
+        category = input("Choose a category for search: ")
+        pattern = input("Enter a search pattern: ")
+        result = self.book.search(pattern, category)
+        for account in result:
+            if account["birthday"]:
+                birth = account["birthday"].strftime("%d/%m/%Y")
+                result = (
+                    "_" * 50
+                    + "\n"
+                    + f"Name: {account['name']} \nPhones: {', '.join(account['phones'])} \nBirthday: {birth} \nEmail: {account['email']} \nStatus: {account['status']} \nNote: {account['note']}\n"
+                    + "_" * 50
+                )
+                print(result)
+
+    def edit_contact(self):
+        contact_name = input("Contact name: ")
+        parameter = input(
+            "Which parameter do you want to change (name, phones, birthday, status, email, note): "
+        ).strip()
+        new_value = input("New value: ")
+        self.book.edit(contact_name, parameter, new_value)
+
+    def remove_contact(self):
+        pattern = input("Delete (contact name or phone number): ")
+        self.book.remove(pattern)
+
+    def save_to_file(self):
+        file_name = input("File name: ")
+        self.book.save(file_name)
+
+    def load_from_file(self):
+        file_name = input("File name: ")
+        self.book.load(file_name)
+
+    def congratulate(self):
+        print(self.book.congratulate())
+
+    def view_contacts(self):
+        print(self.book)
+
+
+class UserInterface:
+    def __init(self):
+        self.bot = Bot()
+
+    def run(self):
+        while True:
+            action = input("Enter a command: ")
+            self.bot.handle(action)
+            if action == "exit":
+                break
+
+
+if __name__ == "__main__":
+    interface = UserInterface()
+    interface.run()
